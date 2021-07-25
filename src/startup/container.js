a1e39c636238fd68460f7e733c09f8d498b3ba15
German Ramirez <mancho@Germans-MacBook-Pro.local>
2021-07-24 20:16:30 -0500

    ADD: services added

--- src/startup/container.js
+++ src/startup/container.js
@@ -5,7 +5,7 @@ const config = require('../config');
 const app = require('.');
 
 //services
-const { HomeService } = require('../services')
+const { HomeService, UserService, IdeaService, CommentService } = require('../services')
 
 //controllers
 const { HomeController } = require('../controllers')
@@ -23,12 +23,15 @@ const { UserRepository, IdeaRepository, CommentRepository } = require('../reposi
 const container = createContainer();
 
 container.register({
-        app: asClass(app).singleton(),
-        router: asFunction(Routes).singleton(),
-        config: asValue(config)
-    })
+    app: asClass(app).singleton(),
+    router: asFunction(Routes).singleton(),
+    config: asValue(config)
+})
     .register({
-        HomeService: asClass(HomeService).singleton()
+        HomeService: asClass(HomeService).singleton(),
+        UserService: asClass(UserService).singleton(),
+        IdeaService: asClass(IdeaService).singleton(),
+        CommentService: asClass(CommentService).singleton(),
     })
     .register({
         HomeController: asClass(HomeController.bind(HomeController)).singleton()
